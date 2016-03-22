bf1fda5701d2605f459f3eaf1cead4fa9340141b
Andrey Molchanov <neodelf@gmail.com>
2016-03-23 00:40:27 +0300

    [1] set locale only for GET requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_locale
+  before_action :set_locale, if: :is_get_request?
 
   private
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
     Rails.application.routes.default_url_options[:locale] = I18n.locale
+  end
+
+  def is_get_request?
+    request.request_method == 'GET'
   end
 end
