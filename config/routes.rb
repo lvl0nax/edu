8f142c979934b3613da1c84a2bfbe44a6eea1854
Andrey Molchanov <neodelf@gmail.com>
2016-03-24 01:35:41 +0300

    [fix] fixes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   scope '/:locale', locale: /#{I18n.available_locales.join('|')}/ do
     root to: 'welcome#home'
-    get '/*path', to: redirect('/')
   end
   root to: redirect("/#{I18n.default_locale}", status: 302), as: :without_locale_root
-  get '/*path', to: redirect("/#{I18n.default_locale}/%{path}", status: 302), format: false
+  get '/*path', to: redirect("/#{I18n.default_locale}/%{path}", status: 302),
+      constraints: {path: /(?!((#{I18n.available_locales.join("|")}|assets))\/).*/}, format: false
 end
