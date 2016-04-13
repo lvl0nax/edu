7c4325afcdb09be8e7f6f73ffac1ee826fa23ab3
lvl0nax <lvl0nax@gmail.com>
2016-04-13 15:47:08 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :log_level, :info
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w(bin log tmp/cache vendor/bundle public/system public/uploads)
+set :linked_dirs, %w(config bin log tmp/cache vendor/bundle public/system public/uploads)
 
 # Default value for keep_releases is 5
 set :keep_releases, 3
